dbdc4fb4756b68646e9351ad985a73f5edea79a7
BO MI KANG <kangbm92@gmail.com>
2022-12-21 15:19:16 +0900

    Bomi (#11)
    * 22-12-21 강보미 StudyMapper 안의 resultMap생성/ delete Test Case작성중 중간 merge
    
    * 22-12-21 강보미 StudyRepositoryTest delete Test완료
    
    Co-authored-by: NA YEONG KIM <kny0339@gmail.com>

--- dogroup/src/test/java/com/dogroup/repository/StudyRepositoryTest.java
+++ dogroup/src/test/java/com/dogroup/repository/StudyRepositoryTest.java
@@ -2,12 +2,16 @@ package com.dogroup.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +20,7 @@ import org.springframework.test.context.ContextConfiguration;
 import com.dogroup.DogroupApplication;
 import com.dogroup.config.MyApplicationContext;
 import com.dogroup.dto.StudyDTO;
+import com.dogroup.dto.StudySubjectDTO;
 import com.dogroup.dto.StudyUserDTO;
 import com.dogroup.dto.UserDTO;
 import com.dogroup.exception.AddException;
@@ -33,8 +38,9 @@ class StudyRepositoryTest {
 	@Autowired
 	UserRepository userRepository;
 
-//	@Test
-
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
 	// @Test
 	/**
 	 * @author NYK
@@ -143,6 +149,10 @@ class StudyRepositoryTest {
 	 * @throws FindException
 	 */
 //	@Test
+
+	
+	//@Test
+
 	void 스터디정보가져오기() throws FindException {
 		int studyId = 55;
 		StudyDTO study = studyRepository.selectStudyByStudyId(studyId);
@@ -163,8 +173,12 @@ class StudyRepositoryTest {
 
 	}
 
-//	@Test
-	/**
+
+
+
+	//@Test
+	/**Homework를 insert하는 Test Case
+
 	 * @author kangb
 	 * @throws AddException
 	 */
@@ -176,8 +190,21 @@ class StudyRepositoryTest {
 		assertNotNull(created_at);
 
 	}
+	
+	/**
+	 * @author kangb
+	 * 스터디ID에 해당하는 과목들을 삭제하는 Test Case
+	 */
 
 	@Test
+	 void deleteStudySubject() throws RemoveException{
+		SqlSession session = sqlSessionFactory.openSession();
+		studyRepository.deleteStudySubject(session, 80);		
+	
+	}
+	
+	
+	//@Test
 	void 스터디삭제() {
 		try {
 			studyRepository.deleteStudy(79);

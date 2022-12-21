bf535e180af159677af0a790d519834907913bb1
NYK <kny0339@gmail.com>
2022-12-21 14:50:50 +0900

    2022-12-21 김나영 컨트롤러 일부 수정(pathvariable에서 email 제외)

--- dogroup/src/main/java/com/dogroup/util/GithubLoginUtil.java
+++ dogroup/src/main/java/com/dogroup/util/GithubLoginUtil.java
@@ -42,7 +42,7 @@ public class GithubLoginUtil {
 	 * USER_EMAIL_URL : ACCESS_TOKEN_URL에의 요청을 통해 발급받은 Access Token으로 user email을 요청한다.
 	 */
 	public static final String AUTHORIZE_URL = "https://github.com/login/oauth/authorize";
-	public static final String REDIRECT_URL = "/auth/github/callback";
+	public static final String REDIRECT_URL = "/user/auth/github/callback";
 	public static final String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
 	public static final String USER_EMAIL_URL = "https://api.github.com/user/emails";
 	
